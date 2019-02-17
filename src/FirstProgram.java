e3d66c18b5e76e752a166cb28d1f4e52f273e85f
Nitesh Kumar N <niteshkumarn55@gmail.com>
2019-02-17 21:07:00 +0530

    Integrating the first commit and the second commit features

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class FirstProgram {
 
+
+    public void addNdPrintValuesInArrays(){
+        int[] a = new int[10];
+
+        for(int i=0; i<10 ; i++){
+            System.out.println("adding the value for the array index location "+i);
+            a[i] = i+1;
+        }
+        int i=1;
+        System.out.println("The lenght of the array "+a.length);
+        while (i<=a.length){
+            System.out.println(a[i-1]);
+            i++;
+        }
+    }
+
     public void addCalc(int a, int b){
         int c =  a+b;
         System.out.println("The added value is "+c);
@@ -10,6 +26,7 @@ public class FirstProgram {
 
     public static void main(String[] args) {
         FirstProgram f1 = new FirstProgram();
+        f1.addNdPrintValuesInArrays();
         f1.addCalc(5,5);
     }
 }
