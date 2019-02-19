dec0568135e22ffb10d891704b165956b0c4b00b
Nitesh Kumar N <niteshkumarn55@gmail.com>
2019-02-20 01:51:13 +0530

    2-dimensional code example

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -18,6 +18,25 @@ public class FirstProgram {
         }
     }
 
+    public void addValuesToTwoDimension(){
+        int[][] a = new int[3][6];
+        // Adding the values in the 2dimensional array
+        for(int i=0; i<3; i++){
+            for(int j=0; j<6; j++){
+                a[i][j] = j*2;
+            }
+        }
+
+        // printing the values from the 2dimnesional array
+        for(int i=0; i<3; i++){
+            for(int j=0; j<6; j++){
+                System.out.println("for the row -> "+i+" and for the column ->"+j+" the inserted values are -> "+a[i][j]);
+            }
+        }
+
+
+    }
+
     public void addCalc(int a, int b){
         int c =  a+b;
         System.out.println("The added value is "+c);
@@ -26,7 +45,8 @@ public class FirstProgram {
 
     public static void main(String[] args) {
         FirstProgram f1 = new FirstProgram();
-        f1.addNdPrintValuesInArrays();
-        f1.addCalc(10,40);
+     /*   f1.addNdPrintValuesInArrays();
+        f1.addCalc(10,40);*/
+        f1.addValuesToTwoDimension();
     }
 }
